4fdc01c0093c80862380935f71a33eae87ba455e
vinhct <vinhfalcao@gmail.com>
2018-06-01 14:46:39 +0700

    addd

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://xengclub.dev/';
+$config['base_url'] = 'http://localhost:8080/new2018/';
 $config['api_url'] = 'http://portal.vinplay.com/api';
 /*
 |--------------------------------------------------------------------------
